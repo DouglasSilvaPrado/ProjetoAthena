9a0599bb67b5c5eae3a6e155aeed5b80c50bb6a4
Manoel Ailson <94858476+manoelailson@users.noreply.github.com>
2021-12-23 15:33:16 -0300

    PARTE2

---
+++ Squad55/Utils/Hash.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Squad55.Utils
+{
+    public class Hash
+    {
+        public static string GerarHash(string texto) 
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            byte[] bytes = Encoding.UTF8.GetBytes(texto);
+            byte[] hash = sha256.ComputeHash(bytes);
+            StringBuilder result = new StringBuilder();
+            for (int i=0;i<hash.Length;i++) {
+                result.Append(hash[i].ToString("X"));
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ Squad55/ViewModels/AlterarSenhaViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Squad55.ViewModels
+{
+    public class AlterarSenhaViewModel
+    {
+        [Required(ErrorMessage = "Informe sua senha atual")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha atual")]
+        [MinLength(6, ErrorMessage = "A senha deve ter pelo menos 6 caracteres")]
+        public string SenhaAtual { get; set; }
+
+        [Required(ErrorMessage = "Informe sua nova senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nova senha")]
+        [MinLength(6, ErrorMessage = "A nova senha deve ter pelo menos 6 caracteres")]
+        public string NovaSenha { get; set; }
+
+
+        [Required(ErrorMessage = "Confirme sua nova senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar senha")]
+        [MinLength(6, ErrorMessage = "A senha deve ter pelo menos 6 caracteres")]
+        [Compare(nameof(NovaSenha), ErrorMessage = "A senha e a confirmação não estão iguais")]
+        public string ConfirmacaoSenha { get; set;}
+
+    }
+}
