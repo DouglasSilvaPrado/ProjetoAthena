687b4b1a852fa0e9725a0c8cbaad2fa311323270
Manoel Ailson <94858476+manoelailson@users.noreply.github.com>
2021-12-23 15:35:57 -0300

    PARTE4

---
+++ Squad55/Controllers/PainelController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Squad55.Controllers
+{
+    public class PainelController : Controller
+    {
+       [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Mensagens()
+        {
+            return View();
+        }
+    }
+}
