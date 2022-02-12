f06787aff1e343228ec36a3fbabe4ec1a95d04e2
Guilherme Martins <guis.1177@gmail.com>
2022-02-11 23:13:08 -0300

    Rota Cursos

---
+++ Squad55/Controllers/CursosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Squad55.Controllers
+{
+    public class CursosController : Controller
+    {
+       
+
+        public ActionResult CursoMarketingDigital()
+        {
+
+            return View();
+        }
+
+       
+    }
+}
